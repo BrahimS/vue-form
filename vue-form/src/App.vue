54950fe71347e625d1bcfe355b90999e5534a94e
BS <bs@bss-mac-pro.home>
2020-02-26 20:49:59 +0100

    add list

---
+++ vue-form/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <h2>hello vueJs</h2>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptatum minus ratione consequatur similique sit suscipit, ea voluptas impedit cumque inventore ipsam dolorum sapiente molestias sint quaerat rem assumenda necessitatibus.</p>
+  
+      <List 
+      itemType="CITY"
+      :listItems="['Marrakech', 'Rabat', 'Tanger', 'Asila']" />
+      
+      <List 
+      itemType="FRUIT"
+      :listItems="['Orange', 'Pomme', 'Bananne', 'Poire']" />
+  
+  </div>
+
+
+
+</template>
+
+<script>
+import List from './List';
+export default {
+  components: {List}
+}
+</script>
---
+++ vue-form/src/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <h2>{{itemType}}</h2>
+    <ul>
+      <li v-for="(item, index) in listItems" :key="index">
+        {{item}}
+      </li>
+    </ul>
+    <p v-if="listItems.length > 4">There are {{listItems.length}} {{itemType.toLowerCase()}}</p>
+    <p v-else>There are no {{itemType.toLowerCase()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listItems: {
+      default: ''
+    },
+      itemType: {
+      default: () => []
+    },
+  }
+  
+}
+</script>

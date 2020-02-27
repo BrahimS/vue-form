4c9d9b647ee38689edbe071f9727fea1e145e740
BS <bs@bss-mac-pro.home>
2020-02-27 11:12:15 +0100

    fetch data with axios

--- vue-list/src/App.vue
+++ vue-list/src/App.vue
@@ -2,24 +2,34 @@
   <div>
    <h2>hello vueJs</h2>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptatum minus ratione consequatur similique sit suscipit, ea voluptas impedit cumque inventore ipsam dolorum sapiente molestias sint quaerat rem assumenda necessitatibus.</p>
-  
       <List 
       itemType="Cities"
       :listItems="['Marrakech', 'Rabat', 'Tanger', 'Asila']" />
       
       <List 
       itemType="Fruit"
-      :listItems="['Orange', 'Pomme', 'Bananne', 'Poire']" />
+      :listItems="cities" />
   
   </div>
-
-
-
 </template>
 
 <script>
 import List from './List';
+import axios from 'axios';
 export default {
-  components: {List}
+  data() {
+    return {
+      cities: []
+    };
+  },
+  components: {List},
+  // Vue lifecycle events: 
+  created() {
+    // fetch data with axios
+    axios.get('/cities.json').then(res => {
+      this.cities = res.data.items;
+      console.log(this.cities);
+    })
+  }
 }
 </script>
